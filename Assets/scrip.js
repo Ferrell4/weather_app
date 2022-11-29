cb3fa71691dbf404496a82d3276536dbc5d82a4f
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-11-28 20:23:43 -0500

    added api

--- Assets/scrip.js
+++ Assets/scrip.js
@@ -1,13 +1,19 @@
-var getCityWeather = function(city) {
-
-    var apiKey="30ba22cf3476c2ad903f09a213b675ab"
-    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
-    fetch(apiURL)
-        .then(function (response) {
-            response.json().then(function(data) {
-                displayWeather(data, city);
-            });
-        });
-        
-};
+function weatherApp (){
+        //this is to retrieve weather from open weather 
+        function getWeather(city) {
+            var apiKey = "34ad3da7d04d1aca98c93ff617bbb6e8";
+            var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
+            // https://api.openweathermap.org/data/2.5/weather?q=city&appid=05027a18faadbe64fa845a2851972b89
+    
+            fetch(apiUrl)
+                .then(function (response) {
+                    console.log(response)
+                    return response.json()
+    
+                })
+                .then(function (data) {
+                    console.log(data);
+                    currentWeather(data, city);
+                });
+        }
+}
